f71a6ba77bb003d3983b6328ddc13e1abb8b6e9f
Adrien Jones <tjones69@memphis.edu>
2024-03-14 20:16:01 -0500

    Implemented Conjured class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,7 +11,6 @@ class GildedRose
     end
   end
 
-
   class Normal < Item
     def tick
       @days_remaining -= 1
@@ -42,11 +41,22 @@ class GildedRose
     end
   end
 
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -=2 if days_remaining <= 0
+    end
+  end
+
   DEFAULT_CLASS = Item
   SPECIALIZED_CLASSES = {
     'Normal Item'                               => Normal,
     'Aged Brie'                                 => Brie,
-    'Backstage passes to a TAFKAL80ETC concert' => Backstage
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake'                        => Conjured
   }
 
   def self.new(name:, days_remaining:, quality:)
